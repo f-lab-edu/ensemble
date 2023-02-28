44be7d5205a56ee69736a60946ec51314b736065
naji0001 <najiwon001@gmail.com>
2023-02-28 20:43:19 +0900

    feat: Implement home

--- api/firebase.js
+++ api/firebase.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getFirestore, collection, getDocs, addDoc, doc, updateDoc, deleteDoc, Timestamp,
+} from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAhXikJbMNO2HCGrdYbs0vKLR_KP8BS67M',
   authDomain: 'ensemble-4ba6f.firebaseapp.com',
@@ -13,7 +13,82 @@ const firebaseConfig = {
   appId: '1:264379873721:web:1d88ec51964e5e23869e68',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const postCollection = collection(db, 'post');
 
-export default app;
+const fetchData = async (target) => {
+  try {
+    const posts = await getDocs(postCollection);
+    posts.forEach((post) => {
+      const { title, contents } = post.data();
+      const $li = document.createElement('li');
+      $li.className = 'post-container';
+      $li.innerHTML = `
+        <a>
+          <div class="post-title">${title}</div>
+          <div class="post-body">${contents}</div>
+        </a>  
+      `;
+      target.appendChild($li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const setData = async (uid) => {
+  await addDoc(postCollection, {
+    title: '스터디 모집합니다.',
+    contents: '모각코',
+    writer: uid,
+    hits: 0,
+    tags: ['서울', 'javascript'],
+    rgt_dt: Timestamp.fromDate(new Date(2023, 1, 28)),
+    deadline: Timestamp.fromDate(new Date(2023, 2, 10)),
+  });
+};
+
+const updateData = async (id) => {
+  const data = doc(db, 'post', id);
+  await updateDoc(data, {
+    title: '안녕하세요',
+  });
+};
+
+const deleteData = async (id) => {
+  await deleteDoc(doc(db, 'post', id));
+};
+
+const createUser = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log(userCredential.user);
+    })
+    .catch(() => {
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+    });
+};
+
+const signIn = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const { user } = userCredential;
+      console.log(user);
+    })
+    .catch(() => {
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+    });
+};
+
+export {
+  fetchData,
+  setData,
+  updateData,
+  deleteData,
+  createUser,
+  signIn,
+};
--- main.js
+++ main.js
@@ -1,33 +1,4 @@
-import {
-  getFirestore, collection, getDocs, setDoc, doc, Timestamp,
-} from 'firebase/firestore';
+import { fetchData } from './api/firebase';
 
-import firebase from './api/firebase';
-
-const db = getFirestore(firebase);
-const post = collection(db, 'post');
-const fetchData = async () => {
-  try {
-    const querySnapshot = await getDocs(post);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-fetchData();
-
-const setData = async () => {
-  await setDoc(doc(post, '3'), {
-    title: '스터디 모집',
-    contents: '서울에서 스터디하실분 구해요',
-    writer: 'Lee',
-    hits: 0,
-    tags: ['서울', 'JAVA'],
-    rgt_dt: Timestamp.fromDate(new Date(2023, 1, 28)),
-    deadline: Timestamp.fromDate(new Date(2023, 2, 10)),
-  });
-};
-
-// setData();
+const $postList = document.querySelector('.post-list ul');
+fetchData($postList);
