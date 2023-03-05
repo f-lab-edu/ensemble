82a40707a0a7d7093e442c410c0f3f9f326aded2
naji0001 <najiwon001@gmail.com>
2023-03-05 23:14:39 +0900

    refactor: Refactor post component

--- src/components/PostComponent.js
+++ src/components/PostComponent.js
@@ -1,28 +1,39 @@
+import createElement from '../util/util';
 import { fetchData } from '../../api/firebase';
 
 const Post = () => {
-  const $post = document.createElement('div');
-  const $headerSection = document.createElement('section');
-  $headerSection.className = 'community-header';
-  $headerSection.innerHTML = `
-    <p>함께 성장할 스터디를 모집해보세요</p>
-    <a href="/postwrite" data-link>글쓰기</a>
-  `;
-  const $postSection = document.createElement('section');
-  $postSection.className = 'post-list';
-  const $ul = document.createElement('ul');
+  const $post = createElement('div');
+  const $headerSection = localStorage.getItem('user')
+    ? createElement(
+      'section',
+      `
+        <p>함께 성장할 스터디를 모집해보세요</p>
+        <a href="/postwrite" data-link>글쓰기</a>
+      `,
+      'community-header',
+    )
+    : createElement(
+      'section',
+      '<p>함께 성장할 스터디를 모집해보세요</p>',
+      'community-header',
+    );
+  const $postSection = createElement('section', '', 'post-list');
+  const $ul = createElement('ul');
+
   fetchData()
     .then((posts) => {
       posts.forEach((post) => {
         const { title, contents } = post.data();
-        const $li = document.createElement('li');
-        $li.className = 'post-container';
-        $li.innerHTML = `
+        const $li = createElement(
+          'li',
+          `
             <a>
               <div class="post-title">${title}</div>
               <div class="post-body">${contents}</div>
             </a>
-          `;
+          `,
+          'post-container',
+        );
         $ul.append($li);
       });
       $postSection.append($ul);
